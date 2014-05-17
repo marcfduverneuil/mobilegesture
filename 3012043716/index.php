e430e269d71367022512d5259ce7a3b95edfc92c
mobilegesture <S3pt3mb3r11>
2014-05-17 19:17:51 -0400

    test301
    Test scripting for mobile site

---
+++ 3012043716/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>stuff</title>
+<script src="script/jquery.mobile.custom.js" type="text/javascript" charset="utf-8"></script>
+<link rel="stylesheet" href="script/jquery.mobile.custom.structure.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="script/jquery.mobile.custom.theme.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<body>
+	<div class="ui-mobile">		
+		test
+	</div>
+</body>
+</html>
